31639a524ac814b9e1bb0d97c4f2d3155c09ffcb
Samodra <38998978+saamodra@users.noreply.github.com>
2021-02-10 23:02:36 +0700

    db

--- db_sekolahanakbangsa.sql
+++ db_sekolahanakbangsa.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 09, 2021 at 05:40 PM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 7.4.13
+-- Generation Time: Feb 10, 2021 at 05:01 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -51,6 +51,13 @@ CREATE TABLE `aksi` (
   `row_status` enum('A','D') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `aksi`
+--
+
+INSERT INTO `aksi` (`id_aksi`, `id_relawan`, `nama_aksi`, `target_donasi`, `deskripsi_aksi`, `tanggal_selesai`, `creaby`, `creadate`, `modiby`, `modidate`, `row_status`) VALUES
+(6, 12, 'Bantu anak pesantren', '130000', 'Test', '2021-02-25 00:00:00', 'teddy', '2021-02-10 20:01:31', 'teddy', '2021-02-10 20:01:51', 'A');
+
 -- --------------------------------------------------------
 
 --
@@ -70,6 +77,13 @@ CREATE TABLE `aksi_barang` (
   `row_status` enum('A','D') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `aksi_barang`
+--
+
+INSERT INTO `aksi_barang` (`id`, `id_aksi`, `id_barang`, `jumlah`, `harga_satuan`, `creaby`, `creadate`, `modiby`, `modidate`, `row_status`) VALUES
+(3, 6, 13, 4, 25000, 'teddy', '2021-02-10 20:01:31', 'teddy', '2021-02-10 20:01:51', 'A');
+
 -- --------------------------------------------------------
 
 --
@@ -88,6 +102,13 @@ CREATE TABLE `aksi_biaya_lainnya` (
   `row_status` enum('A','D') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `aksi_biaya_lainnya`
+--
+
+INSERT INTO `aksi_biaya_lainnya` (`id`, `id_aksi`, `id_biaya_lainnya`, `biaya`, `creaby`, `creadate`, `modiby`, `modidate`, `row_status`) VALUES
+(1, 6, 16, 30000, 'teddy', '2021-02-10 20:01:31', 'teddy', '2021-02-10 20:01:51', 'A');
+
 -- --------------------------------------------------------
 
 --
@@ -104,6 +125,15 @@ CREATE TABLE `barang` (
   `modidate` datetime NOT NULL DEFAULT current_timestamp(),
   `row_status` enum('A','D') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `barang`
+--
+
+INSERT INTO `barang` (`id_barang`, `nama_barang`, `deskripsi_barang`, `creaby`, `creadate`, `modiby`, `modidate`, `row_status`) VALUES
+(13, 'Meja', 'Meja', 'Alvin Amartya', '2021-02-10 19:54:03', 'Alvin Amartya', '2021-02-10 19:54:03', 'A'),
+(14, 'P3K', 'P3K', 'Alvin Amartya', '2021-02-10 19:54:17', 'Alvin Amartya', '2021-02-10 19:54:17', 'A'),
+(15, 'Kursi', 'Kursi', 'Alvin Amartya', '2021-02-10 19:54:25', 'Alvin Amartya', '2021-02-10 19:54:25', 'A');
 
 -- --------------------------------------------------------
 
@@ -122,6 +152,14 @@ CREATE TABLE `biaya_lainnya` (
   `row_status` enum('A','D') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `biaya_lainnya`
+--
+
+INSERT INTO `biaya_lainnya` (`id_biaya_lainnya`, `nama_biaya_lainnya`, `deskripsi_biaya_lainnya`, `creaby`, `creadate`, `modiby`, `modidate`, `row_status`) VALUES
+(15, 'Renovasi Genteng', 'Renovasi Genteng', 'Alvin Amartya', '2021-02-10 19:54:55', 'Alvin Amartya', '2021-02-10 19:54:55', 'A'),
+(16, 'Renovasi Lantai', 'Renovasi Lantai', 'Alvin Amartya', '2021-02-10 19:55:06', 'Alvin Amartya', '2021-02-10 19:55:06', 'A');
+
 -- --------------------------------------------------------
 
 --
@@ -138,6 +176,14 @@ CREATE TABLE `cluster_relawan` (
   `modidate` datetime NOT NULL DEFAULT current_timestamp(),
   `row_status` enum('A','D') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `cluster_relawan`
+--
+
+INSERT INTO `cluster_relawan` (`id_cluster_relawan`, `nama_cluster`, `deskripsi_cluster`, `creaby`, `creadate`, `modiby`, `modidate`, `row_status`) VALUES
+(7, 'Guru', 'Guru', 'Alvin Amartya', '2021-02-10 19:55:27', 'Alvin Amartya', '2021-02-10 19:55:27', 'A'),
+(8, 'Medis', 'Medis', 'Alvin Amartya', '2021-02-10 19:56:14', 'Alvin Amartya', '2021-02-10 19:56:14', 'A');
 
 -- --------------------------------------------------------
 
@@ -158,6 +204,13 @@ CREATE TABLE `donatur` (
   `modidate` datetime NOT NULL DEFAULT current_timestamp(),
   `row_status` enum('A','D') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `donatur`
+--
+
+INSERT INTO `donatur` (`id_donatur`, `id_user_login`, `nama_donatur`, `email_donatur`, `jenis_kelamin`, `no_telepon`, `creaby`, `credate`, `modiby`, `modidate`, `row_status`) VALUES
+(8, 33, 'Ivan', 'ivan@gmail.com', 'L', '081231723123', '-', '2021-02-10 19:58:47', '-', '2021-02-10 19:58:47', 'A');
 
 -- --------------------------------------------------------
 
@@ -182,6 +235,14 @@ CREATE TABLE `donatur_aksi` (
   `row_status` enum('A','D') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `donatur_aksi`
+--
+
+INSERT INTO `donatur_aksi` (`id`, `id_donatur`, `id_aksi`, `id_status_aksi`, `donasi`, `bukti_transfer`, `is_valid`, `is_anonum`, `keterangan`, `creaby`, `creadate`, `modiby`, `modidate`, `row_status`) VALUES
+(7, 8, 6, 3, 100000, 'fort_rotterdam.jpg', 'Y', 'Y', 'Test', 'Ivan', '2021-02-10 20:03:34', 'Alvin Amartya', '2021-02-10 20:03:34', 'A'),
+(8, 8, 6, 2, 10000, 'jendela_alam.jpg', NULL, 'Y', 'Semoga bermanfaat', 'Ivan', '2021-02-10 22:59:53', 'Ivan', '2021-02-10 22:59:53', 'A');
+
 -- --------------------------------------------------------
 
 --
@@ -198,6 +259,14 @@ CREATE TABLE `gambar_aksi` (
   `modidate` datetime NOT NULL DEFAULT current_timestamp(),
   `row_status` enum('A','D') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `gambar_aksi`
+--
+
+INSERT INTO `gambar_aksi` (`id`, `id_aksi`, `gambar`, `creaby`, `creadate`, `modiby`, `modidate`, `row_status`) VALUES
+(8, 6, '202102100801510.jpg', 'teddy', '2021-02-10 20:01:52', 'teddy', '2021-02-10 20:01:52', 'A'),
+(9, 6, '2021021008015101.jpg', 'teddy', '2021-02-10 20:01:52', 'teddy', '2021-02-10 20:01:52', 'A');
 
 -- --------------------------------------------------------
 
@@ -228,7 +297,8 @@ CREATE TABLE `karyawan` (
 --
 
 INSERT INTO `karyawan` (`id_karyawan`, `id_user_login`, `nama_karyawan`, `jenis_kelamin`, `jabatan_karyawan`, `nik`, `no_telepon`, `email`, `tempat_lahir`, `tanggal_lahir`, `creaby`, `creadate`, `modiby`, `modidate`, `row_status`) VALUES
-(7, 1, 'Alvin Amartya', 'L', 'Super Admin', '1123156785431236-', '08123456789', 'alvinamartya@gmail.com', 'Bengkulu', '2000-05-02 23:38:47', '', '2021-02-09 23:40:07', '', '2021-02-09 23:40:07', 'A');
+(7, 1, 'Alvin Amartya', 'L', 'Super Admin', '1123156785431236-', '08123456789', 'alvinamartya@gmail.com', 'Bengkulu', '2000-05-02 23:38:47', '', '2021-02-09 23:40:07', '', '2021-02-09 23:40:07', 'A'),
+(8, 32, 'Rizki', 'L', 'Admin', '0192831723871823', '09172387123', 'rizki@gmail.com', 'Jakarta', '2000-01-01 00:00:00', 'Alvin Amartya', '2021-02-10 19:57:59', 'Alvin Amartya', '2021-02-10 19:57:59', 'A');
 
 -- --------------------------------------------------------
 
@@ -243,7 +313,7 @@ CREATE TABLE `kebutuhan_tahunan` (
   `tahun` char(4) NOT NULL,
   `total_kebutuhan` decimal(10,0) NOT NULL,
   `deskripsi` varchar(255) NOT NULL,
-  `is_approved` enum('Y','N') DEFAULT NULL,
+  `kt_status` enum('Draft','Menunggu Persetujuan','Disetujui','Ditolak') NOT NULL DEFAULT 'Draft',
   `laporan_pertanggung_jawaban` varchar(100) DEFAULT NULL,
   `creaby` varchar(100) NOT NULL,
   `creadate` datetime NOT NULL DEFAULT current_timestamp(),
@@ -251,6 +321,15 @@ CREATE TABLE `kebutuhan_tahunan` (
   `modidate` datetime NOT NULL DEFAULT current_timestamp(),
   `row_status` enum('A','D') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `kebutuhan_tahunan`
+--
+
+INSERT INTO `kebutuhan_tahunan` (`id`, `id_relawan`, `id_sekolah`, `tahun`, `total_kebutuhan`, `deskripsi`, `kt_status`, `laporan_pertanggung_jawaban`, `creaby`, `creadate`, `modiby`, `modidate`, `row_status`) VALUES
+(48, 12, 11, '2022', '20000', 'Oke ', 'Disetujui', '20210210080933.xlsx', 'teddy', '2021-02-10 20:02:44', 'teddy', '2021-02-10 20:09:33', 'A'),
+(49, 12, 11, '2023', '260000', 'Kebutuhan tahunan', 'Menunggu Persetujuan', NULL, 'teddy', '2021-02-10 20:52:21', 'teddy', '2021-02-10 20:52:21', 'A'),
+(50, 12, 11, '2021', '25000', 'asd', 'Menunggu Persetujuan', NULL, 'teddy', '2021-02-10 22:34:05', 'teddy', '2021-02-10 22:34:05', 'A');
 
 -- --------------------------------------------------------
 
@@ -271,6 +350,14 @@ CREATE TABLE `kt_barang` (
   `row_status` enum('A','D') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `kt_barang`
+--
+
+INSERT INTO `kt_barang` (`id`, `id_kt`, `id_barang`, `jumlah`, `harga_satuan`, `creaby`, `creadate`, `modiby`, `modidate`, `row_status`) VALUES
+(4, 48, 13, 4, 25000, 'teddy', '2021-02-10 20:02:44', 'teddy', '2021-02-10 21:36:42', 'D'),
+(5, 49, 13, 5, 50000, 'teddy', '2021-02-10 20:52:21', 'teddy', '2021-02-10 21:57:46', 'A');
+
 -- --------------------------------------------------------
 
 --
@@ -288,6 +375,15 @@ CREATE TABLE `kt_biaya_lainnya` (
   `modidate` datetime NOT NULL DEFAULT current_timestamp(),
   `row_status` enum('A','D') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `kt_biaya_lainnya`
+--
+
+INSERT INTO `kt_biaya_lainnya` (`id`, `id_kt`, `id_biaya_lainnya`, `biaya`, `creaby`, `creadate`, `modiby`, `modidate`, `row_status`) VALUES
+(8, 48, 16, 20000, 'teddy', '2021-02-10 20:02:44', 'teddy', '2021-02-10 21:57:36', 'A'),
+(9, 49, 16, 10000, 'teddy', '2021-02-10 20:52:21', 'teddy', '2021-02-10 21:57:46', 'A'),
+(10, 50, 16, 25000, 'teddy', '2021-02-10 22:34:05', 'teddy', '2021-02-10 22:54:37', 'A');
 
 -- --------------------------------------------------------
 
@@ -314,6 +410,13 @@ CREATE TABLE `relawan` (
   `row_status` enum('A','D') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `relawan`
+--
+
+INSERT INTO `relawan` (`id_relawan`, `id_cluster_relawan`, `id_sekolah`, `id_user_login`, `nik`, `nama_relawan`, `jenis_kelamin`, `no_telepon`, `email`, `tempat_lahir`, `tanggal_lahir`, `creaby`, `creadate`, `modiby`, `modidate`, `row_status`) VALUES
+(12, 7, 11, 34, '0912839172371827', 'teddy', 'L', '00192831927381723', 'teddy@gmail.com', 'Pemalang', '2000-01-15 00:00:00', '-', '2021-02-10 19:59:34', 'Alvin Amartya', '2021-02-10 19:59:34', 'A');
+
 -- --------------------------------------------------------
 
 --
@@ -333,6 +436,14 @@ CREATE TABLE `sekolah` (
   `modidate` datetime NOT NULL DEFAULT current_timestamp(),
   `row_status` enum('A','D') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `sekolah`
+--
+
+INSERT INTO `sekolah` (`id_sekolah`, `nama_sekolah`, `jenis_sekolah`, `alamat`, `provinsi`, `kota`, `creaby`, `creadate`, `modiby`, `modidate`, `row_status`) VALUES
+(11, 'SDN 1 Babadan', 'Rumah Singgah', 'Babadan', 'Jawa Timur', 'Kabupaten Ponorogo', 'Alvin Amartya', '2021-02-10 19:56:37', 'Alvin Amartya', '2021-02-10 19:56:37', 'A'),
+(12, 'SDN 1 Trisono', 'Rumah Singgah', 'Trisono', 'Di Yogyakarta', 'Kabupaten Bantul', 'Alvin Amartya', '2021-02-10 19:57:04', 'Alvin Amartya', '2021-02-10 19:57:04', 'A');
 
 -- --------------------------------------------------------
 
@@ -399,7 +510,10 @@ CREATE TABLE `user_login` (
 --
 
 INSERT INTO `user_login` (`id`, `username`, `password`, `role`, `creaby`, `creadate`, `modiby`, `modidate`, `row_status`) VALUES
-(1, 'sa', '$2y$10$yp1I2KBPVa1TNCq3JTZxheZgQ94GaKtdftJ8047niyjV22br4JSWG', 'Karyawan', 'Alvin Amartya', '2021-01-21 10:39:34', 'Alvin Amartya', '2021-01-21 10:39:34', 'A');
+(1, 'sa', '$2y$10$yp1I2KBPVa1TNCq3JTZxheZgQ94GaKtdftJ8047niyjV22br4JSWG', 'Karyawan', 'Alvin Amartya', '2021-01-21 10:39:34', 'Alvin Amartya', '2021-01-21 10:39:34', 'A'),
+(32, 'rizki', '$2y$10$CbtUV//p/EOoo.3BZ/6c8.zL8fpMCATQEyik7P4hu6XVxaLYHQpIe', 'Karyawan', 'Alvin Amartya', '2021-02-10 19:57:59', 'Alvin Amartya', '2021-02-10 19:57:59', 'A'),
+(33, 'ivan', '$2y$10$L.IeCrLiIlAWet1N9UhYvORmBNYgwEwXgBIagjTs8CHiEatdR.KMW', 'Donatur', '-', '2021-02-10 19:58:47', '-', '2021-02-10 19:58:47', 'A'),
+(34, 'teddy', '$2y$10$ZjcqRJ1djIo4QVJClG/YTufCAhMBszKFYy/UgQs1FUwjMZ8N8.M1i', 'Relawan', '-', '2021-02-10 19:59:34', '-', '2021-02-10 19:59:34', 'A');
 
 --
 -- Indexes for dumped tables
@@ -542,91 +656,91 @@ ALTER TABLE `user_login`
 -- AUTO_INCREMENT for table `aksi`
 --
 ALTER TABLE `aksi`
-  MODIFY `id_aksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_aksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `aksi_barang`
 --
 ALTER TABLE `aksi_barang`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `aksi_biaya_lainnya`
 --
 ALTER TABLE `aksi_biaya_lainnya`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `barang`
 --
 ALTER TABLE `barang`
-  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `biaya_lainnya`
 --
 ALTER TABLE `biaya_lainnya`
-  MODIFY `id_biaya_lainnya` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id_biaya_lainnya` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `cluster_relawan`
 --
 ALTER TABLE `cluster_relawan`
-  MODIFY `id_cluster_relawan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_cluster_relawan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `donatur`
 --
 ALTER TABLE `donatur`
-  MODIFY `id_donatur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_donatur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `donatur_aksi`
 --
 ALTER TABLE `donatur_aksi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `gambar_aksi`
 --
 ALTER TABLE `gambar_aksi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `karyawan`
 --
 ALTER TABLE `karyawan`
-  MODIFY `id_karyawan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_karyawan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `kebutuhan_tahunan`
 --
 ALTER TABLE `kebutuhan_tahunan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
 
 --
 -- AUTO_INCREMENT for table `kt_barang`
 --
 ALTER TABLE `kt_barang`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `kt_biaya_lainnya`
 --
 ALTER TABLE `kt_biaya_lainnya`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `relawan`
 --
 ALTER TABLE `relawan`
-  MODIFY `id_relawan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id_relawan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `sekolah`
 --
 ALTER TABLE `sekolah`
-  MODIFY `id_sekolah` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_sekolah` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `siswa`
@@ -644,7 +758,7 @@ ALTER TABLE `status_aksi`
 -- AUTO_INCREMENT for table `user_login`
 --
 ALTER TABLE `user_login`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
 --
 -- Constraints for dumped tables
